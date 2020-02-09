de30bc142f9e2699ae4e81ba52c57ed951bf1a4e
Pedro <pedrobusou@gmail.com>
2020-02-09 22:09:08 +0100

    Add badge functions to check later

---
+++ src/js/background.js
@@ -0,0 +1,11 @@
+//https://developer.chrome.com/extensions/browserAction#method-setBadgeText
+//chrome.browserAction.setBadgeText({}, () => {});
+
+//https://developer.chrome.com/extensions/browserAction#method-getBadgeText
+//chrome.browserAction.getBadgeText({}, () => {});
+
+//https://developer.chrome.com/extensions/browserAction#method-setBadgeBackgroundColor
+//chrome.browserAction.setBadgeBackgroundColor({}, () => {});
+
+//https://developer.chrome.com/extensions/browserAction#method-getBadgeBackgroundColor
+//chrome.browserAction.getBadgeBackgroundColor({}, () => {});
